7bd67401131503151b8f11b66e77048a1857667e
Wojtazzzz <marcin.witas72@gmail.com>
2025-05-25 01:26:59 +0200

    refactor/remove unused code, comments

--- config/auth.php
+++ config/auth.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Modules\Companies\Domain\Models\Employee;
+
 return [
 
     /*
@@ -64,7 +66,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
+            'model' => env('AUTH_MODEL', Employee::class),
         ],
 
         // 'users' => [
