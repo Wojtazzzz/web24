1c5618bbcd56a5760b58a299851722f8451c222e
Wojtazzzz <marcin.witas72@gmail.com>
2025-05-26 12:03:11 +0200

    feat/add 404 error handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            return response()->json(
+                data: [
+                    'message' => 'Not found',
+                ],
+                status: 404,
+            );
+        });
     })->create();
